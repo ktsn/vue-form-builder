eb0d2b08a3d7f29cc1728e68dc2eb6c8aee48e6c
ktsn <ktsn55@gmail.com>
2017-10-30 23:29:41 +0900

    docs: add the simplest example

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -2,9 +2,11 @@ const path = require('path')
 const fs = require('fs')
 const pug = require('pug')
 const sass = require('node-sass')
+const buble = require('buble')
 
 const base = path.resolve(__dirname, '../docs')
 const destPath = path.resolve(__dirname, '../dist-docs/')
+const examplePath = path.join(base, 'examples')
 const mainPug = path.join(base, 'index.pug')
 const mainScss = path.join(base, 'style.scss')
 
@@ -17,7 +19,12 @@ try {
   outputHtml = pug.renderFile(mainPug, {
     filename: mainPug,
     basedir: base,
-    doctype: 'html'
+    doctype: 'html',
+
+    // helpers
+    exampleTemplate,
+    exampleScript,
+    transpiledExampleScript
   })
 } catch (err) {
   console.error(err.stack)
@@ -44,3 +51,18 @@ sass.render({
     }
   })
 })
+
+function exampleTemplate(name) {
+  const templatePath = path.join(examplePath, name + '.html')
+  return fs.readFileSync(templatePath, 'utf8')
+}
+
+function exampleScript(name) {
+  const scriptPath = path.join(examplePath, name + '.js')
+  return fs.readFileSync(scriptPath, 'utf8')
+}
+
+function transpiledExampleScript(name) {
+  const script = exampleScript(name)
+  return buble.transform(script).code
+}
