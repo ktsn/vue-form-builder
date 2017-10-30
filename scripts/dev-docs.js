ec63a270631164c4819ea3a5b5274880355df54d
ktsn <ktsn55@gmail.com>
2017-10-30 22:53:38 +0900

    docs: build lib for dev at first

--- scripts/dev-docs.js
+++ scripts/dev-docs.js
@@ -2,6 +2,9 @@ const { execSync } = require('child_process')
 const chokidar = require('chokidar')
 const bs = require('browser-sync').create()
 
+execSync('npm run build')
+execSync('cp dist/vue-form-builder.js dist-docs/')
+
 bs.init({
   server: './dist-docs'
 })
