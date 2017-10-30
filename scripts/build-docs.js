15dbb480697d59ea7628d5e777689e17825ef171
ktsn <ktsn55@gmail.com>
2017-10-30 22:53:26 +0900

    docs: add style build script

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,19 +1,46 @@
 const path = require('path')
 const fs = require('fs')
 const pug = require('pug')
+const sass = require('node-sass')
 
 const base = path.resolve(__dirname, '../docs')
 const destPath = path.resolve(__dirname, '../dist-docs/')
-const mainPath = path.join(base, 'index.pug')
+const mainPug = path.join(base, 'index.pug')
+const mainScss = path.join(base, 'style.scss')
 
 if (!fs.existsSync(destPath)) {
   fs.mkdirSync(destPath)
 }
 
-const outputHtml = pug.renderFile(mainPath, {
-  filename: mainPath,
-  basedir: base,
-  doctype: 'html'
-})
+let outputHtml
+try {
+  outputHtml = pug.renderFile(mainPug, {
+    filename: mainPug,
+    basedir: base,
+    doctype: 'html'
+  })
+} catch (err) {
+  console.error(err.stack)
+}
 
-fs.writeFileSync(path.join(destPath, 'index.html'), outputHtml)
+if (outputHtml) {
+  fs.writeFile(path.join(destPath, 'index.html'), outputHtml, err => {
+    if (err) {
+      console.error(err.stack)
+    }
+  })
+}
+
+sass.render({
+  file: mainScss,
+  outputStyle: 'expanded'
+}, (err, output) => {
+  if (err) {
+    return console.error(err.stack)
+  }
+  fs.writeFile(path.join(destPath, 'style.css'), output.css, err => {
+    if (err) {
+      console.error(err.stack)
+    }
+  })
+})
